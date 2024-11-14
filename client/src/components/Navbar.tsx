ce65912291741faacbe12146b4d7e5f305a38226
felix diaz <felixmdm321@gmail.com>
2024-11-14 14:12:45 -0800

    nav bg

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <>
-        <div className="top-0 h-[10vh] font-archivo-narrow text-2xl font-bold">
+        <div className="top-0 h-full bg-slate-200 font-archivo-narrow text-2xl font-bold">
             <div className="flex flex-row space-x-2 w-full">
                 <Link href="/login" className="text-black bg-slate-400 rounded-2xl p-4 m-2 hover:text-white">
                     Login

3747c23ba9bd3983f6da0eefb5d29f20fb62f278
felix diaz <felixmdm321@gmail.com>
2024-12-08 13:19:44 -0800

    comments box, needs backend

--- client/src/components/containers/ContainersPanel.tsx
+++ client/src/components/containers/ContainersPanel.tsx
@@ -2,8 +2,24 @@
 import { useState } from "react";
 import Containers from "./Containers";
 
+interface FormElements extends HTMLFormControlsCollection {
+    comments: HTMLInputElement
+}
+
+interface CommentFormElement extends HTMLFormElement {
+    readonly elements: FormElements
+}
+
 const ContainersPanel = ()  => {
     const [instruction, setInstruction] = useState("No steps generated")
+    const [comments, setComments] = useState<string>("");
+
+    const handleSubmit = (event: React.FormEvent<CommentFormElement>) => {
+        event.preventDefault();
+        const commentValue = event.currentTarget.elements.comments.value;
+        console.log("Submitted Comment:", commentValue);
+        setComments("");
+    };
 
     return (
         <div className="flex flex-col items-center">
@@ -24,16 +40,33 @@ const ContainersPanel = ()  => {
                     <button className="w-[100%] h-[10%] mt-[15%] bg-blue-600 rounded-md font-bold text-white">
                         NEXT
                     </button>
-                    <div>
-                        <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Comments" required />
-                    </div>
-                    <button className="w-[100%] h-[10%] mt-[15%] bg-blue-600 rounded-md font-bold text-white">
-                        SUBMIT
-                    </button>
+                    <form onSubmit={handleSubmit} className="w-full">
+                        <div className="flex flex-col">
+                            <input
+                                id="comments"
+                                name="comments"
+                                type="text"
+                                value={comments}
+                                onChange={(e) => setComments(e.target.value)}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                placeholder="Comments"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full mt-[15%] bg-blue-600 rounded-md font-bold text-white"
+                        >
+                            SUBMIT
+                        </button>
+                    </form>
                 </div>
             </div>
-            <div className="w-[80%] bg-blue-950 h-[20%]">
-                
+            <div className="w-[80%] bg-blue-950">
+                tita?   
+            </div>
+            <div className="w-[80%] h-[300px] bg-red-950">
+                tita?   
             </div>
         </div>
     );
